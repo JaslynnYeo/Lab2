7fc562ad34a7f13b59f5d7ecf879a00cd0aa9fee
jaslynyeo <jaslynykl.23@ichat.sp.edu.sg>
2024-04-25 13:05:35 +0800

    added sort n median

--- Lab2.py
+++ Lab2.py
@@ -1,3 +1,5 @@
+import statistics
+
 def display_main_menu(): 
     print ("Enter some numbers separated by commas (e.g. 5, 67,320)")
 
@@ -18,11 +20,25 @@ def find_min_max(commno_list):
     min_max_list = [min(commno_list), max(commno_list)]
     print (min_max_list)
 
+def sort_num(common_list):
+    sort_list = common_list
+    sort_list.sort()
+    print(sort_list)
+    return sort_list
+
+
+def calc_median(sorted_list):
+    median_value = statistics.median(sorted_list)
+    print (median_value)
+    
+
 def main():
     display_main_menu()
     common_list = get_user_input()
     calc_avg(common_list)
     find_min_max(common_list)
+    sorted_list =  sort_num(common_list)
+    calc_median(sorted_list)
 
 if __name__ == "__main__":
     main()
